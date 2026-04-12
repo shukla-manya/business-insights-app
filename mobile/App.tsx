ff7086c7d44d841555f690da171d8e0735f84af7
MANYA SHUKLA <work.shuklamanya@gmail.com>
2026-04-12 16:17:51 +0530

    Update App.tsx

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { AuthProvider } from "./src/auth/AuthContext";
 import { AppNavigator } from "./src/navigation/AppNavigator";
-
 export default function App() {
   return (
     <SafeAreaProvider>
